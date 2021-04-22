30752220a41ee7c6cfba0272dbb74a9576ef4a4c
LiufanYuanQi <875150049@qq.com>
2021-04-22 17:44:31 +0800

    LeetCode 单词接龙

--- src/main/java/person/liufan/advanced/tree/LadderLength.java
+++ src/main/java/person/liufan/advanced/tree/LadderLength.java
@@ -1,6 +1,6 @@
 package person.liufan.advanced.tree;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @author: liufan
@@ -13,8 +13,80 @@ public class LadderLength {
     public static void main(String[] args) {
 
     }
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
+    /**
+     * 保存单词id
+     */
+    Map<String, Integer> wordId = new HashMap<>();
+    /**
+     * 路径
+     */
+    List<List<Integer>> edge = new ArrayList<>();
+    /**
+     * 节点数量
+     */
+    int nodeNum = 0;
+
+    /**
+     * 图，没用过
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        for (String word : wordList) {
+            addEdge(word);
+        }
+        addEdge(beginWord);
+        if (!wordId.containsKey(endWord)) {
+            return 0;
+        }
+        int[] dis = new int[nodeNum];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
+        dis[beginId] = 0;
+
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(beginId);
+        while (!que.isEmpty()) {
+            int x = que.poll();
+            if (x == endId) {
+                return dis[endId] / 2 + 1;
+            }
+            for (int it : edge.get(x)) {
+                if (dis[it] == Integer.MAX_VALUE) {
+                    dis[it] = dis[x] + 1;
+                    que.offer(it);
+                }
+            }
+        }
         return 0;
+    }
+
+    /**
+     * 建立字典
+     */
+    public void addEdge(String word) {
+        addWord(word);
+        int id1 = wordId.get(word);
+        char[] array = word.toCharArray();
+        int length = array.length;
+        for (int i = 0; i < length; ++i) {
+            char tmp = array[i];
+            array[i] = '*';
+            String newWord = new String(array);
+            addWord(newWord);
+            int id2 = wordId.get(newWord);
+            edge.get(id1).add(id2);
+            edge.get(id2).add(id1);
+            array[i] = tmp;
+        }
+    }
+
+    /**
+     * 建立字典
+     */
+    public void addWord(String word) {
+        if (!wordId.containsKey(word)) {
+            wordId.put(word, nodeNum++);
+            edge.add(new ArrayList<>());
+        }
     }
 }
