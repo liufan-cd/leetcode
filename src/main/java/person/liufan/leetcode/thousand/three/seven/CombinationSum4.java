c1e63656524797e811a3583b3643be2a6a126fbb
LiufanYuanQi <875150049@qq.com>
2021-04-24 09:39:40 +0800

    LeetCode 377 2021年4月24日09:39:23 每日一题

---
+++ src/main/java/person/liufan/leetcode/thousand/three/seven/CombinationSum4.java
@@ -0,0 +1,28 @@
+package person.liufan.leetcode.thousand.three.seven;
+
+/**
+ * @author: liufan
+ * @E-mail: fan.liu@biz-united.com.cn
+ * @package: person.liufan.leetcode.thousand.three.seven
+ * @description: TODO
+ * @date 2021/4/24
+ */
+public class CombinationSum4 {
+    public static void main(String[] args) {
+        CombinationSum4 combinationSum4 = new CombinationSum4();
+        System.out.println(combinationSum4.combinationSum4(new int[]{1, 2, 3}, 4));
+    }
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                int num = nums[j];
+                if (i >= num) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
