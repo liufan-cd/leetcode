5984a31996126934a64ed363be3e6f07baba447d
LiufanYuanQi <875150049@qq.com>
2021-05-17 17:19:41 +0800

    LeetCode 剑指offer 38 字符串的排列

---
+++ src/main/java/person/liufan/offer/thirtyeight/Permutation.java
@@ -0,0 +1,52 @@
+package person.liufan.offer.thirtyeight;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: liufan
+ * @E-mail: fan.liu@biz-united.com.cn
+ * @package: person.liufan.offer.thirtyeight
+ * @description:
+ * 输入一个字符串，打印出该字符串中字符的所有排列。
+ *
+ *
+ *
+ * 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+ * @date 2021/5/17
+ */
+public class Permutation {
+    public static void main(String[] args) {
+        Permutation permutation = new Permutation();
+        System.out.println(Arrays.toString(permutation.permutation("bcc")));
+    }
+    public String[] permutation(String s) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        boolean[] isUsed = new boolean[chars.length];
+        dfs(ans, builder, chars, isUsed);
+        return ans.toArray(new String[ans.size()]);
+    }
+
+    public void dfs(List<String> ans, StringBuilder builder, char[] chars, boolean[] isUsed) {
+        if (builder.length() == chars.length) {
+            ans.add(builder.toString());
+            return;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (i != 0 && chars[i - 1] == chars[i] && !isUsed[i - 1]) {
+                continue;
+            }
+            if (!isUsed[i]) {
+                builder.append(chars[i]);
+                isUsed[i] = true;
+                dfs(ans, builder, chars, isUsed);
+                isUsed[i] = false;
+                builder.deleteCharAt(builder.length() - 1);
+            }
+        }
+    }
+}
