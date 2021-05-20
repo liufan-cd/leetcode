ff005d57858df195465e05fda28d28cd20c1b99e
LiufanYuanQi <875150049@qq.com>
2021-05-20 16:51:35 +0800

    LeetCode 剑指offer 47 礼物的最大价值

---
+++ src/main/java/person/liufan/offer/fortyseven/MaxValue.java
@@ -0,0 +1,44 @@
+package person.liufan.offer.fortyseven;
+
+import person.liufan.common.ArrayUtils;
+
+import java.util.Arrays;
+
+/**
+ * @author: liufan
+ * @E-mail: fan.liu@biz-united.com.cn
+ * @package: person.liufan.offer.fortyseven
+ * @description:
+ * 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/li-wu-de-zui-da-jie-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/5/20
+ */
+public class MaxValue {
+    public static void main(String[] args) {
+        MaxValue maxValue = new MaxValue();
+        int[][] ints = ArrayUtils.parseTwoArray("[1,3,1]," +
+                "  [1,5,1]," +
+                "  [4,2,1]", 3, 3);
+        System.out.println(Arrays.deepToString(ints));
+        System.out.println(maxValue.maxValue(ints));
+    }
+    public int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.max(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
