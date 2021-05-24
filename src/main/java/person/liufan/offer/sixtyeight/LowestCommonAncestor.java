6f7c64da5c0cf1a152f56f70cd00681f3a73c61a
LiufanYuanQi <875150049@qq.com>
2021-05-24 15:22:10 +0800

    LeetCode 剑指offer 68-I 二叉搜索树的最近公共祖先

---
+++ src/main/java/person/liufan/offer/sixtyeight/LowestCommonAncestor.java
@@ -0,0 +1,47 @@
+package person.liufan.offer.sixtyeight;
+
+import person.liufan.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author: liufan
+ * @E-mail: fan.liu@biz-united.com.cn
+ * @package: person.liufan.offer.sixtyeight
+ * @description:
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/5/24
+ */
+public class LowestCommonAncestor {
+    public static void main(String[] args) {
+
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+    public void getPath(TreeNode root, TreeNode target, List<Integer> path) {
+        path.add(root.val);
+        if (root.val > target.val) {
+            getPath(root.left, target, path);
+        } else if (root.val < target.val) {
+            getPath(root.right, target, path);
+        }
+    }
+}
